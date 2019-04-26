2edd0f0e062ceca36fb7e1eca9c7869034c16abe
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-04-26 19:05:48 +0400

    Add files via upload

---
+++ The Mulla/config.js
@@ -0,0 +1,21 @@
+var api = 'http://165.22.157.63:21896';
+var donationAddress = "QPSAfh3Z7b3ae6fuLvRLHMXkNdESaC2aaZTsY3gAcdBKJCWjE629X4hG7KP9u3tHujC1J1QbX32sePu2EHHncjUW7HsPz2bFZC";
+var blockTargetInterval = 60; // enter the block interval in seconds
+var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+var totalSupply =  50000000000000000; // enter the total supply in atomic units
+var symbol = 'QPSA'; // enter the coin's ticker
+var refreshDelay = 30000;
+var whiteTheme = "css/themes/white-theme.css";
+var nightTheme = "css/themes/dark-theme.css";
+
+// pools stats by MainCoins
+var poolsStat = 
+	[
+		
+    ];
+var nodesStat = 
+	[
+        
+		
+    ];
+
---
+++ The Mulla/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+        'api' => 'http://165.22.157.63:21896',
+        'blockTargetInterval' => 60,
+        'coinUnits' => 100000000
+);
+);
+
